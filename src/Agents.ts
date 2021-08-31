3c7b3ee40c810879d8da28869617b8b7716cf381
MoonEvans <89872167+MoonEvans@users.noreply.github.com>
2021-09-01 04:08:13 +0700

    Update Agents.ts

--- src/Agents.ts
+++ src/Agents.ts
@@ -4,7 +4,7 @@ export const Agents: Array<{user: string, password: string}> = [
   {user: 'Adeline', password: '1234'},
   {user: 'Izreal', password: '1234'},
   {user: 'Vincent', password: '1234'},
-  {user: 'Zoe', password: 'D78J27T3'},
+  {user: 'Zoe', password: '1234'},
   {user: 'EuSh', password: 'lawsHammer'}
 ]
 export default Agents;
