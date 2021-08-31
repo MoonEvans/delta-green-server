c016337815120ade6dee2250575655fdc9e612b8
MoonEvans <89872167+MoonEvans@users.noreply.github.com>
2021-09-01 03:40:43 +0700

    Update AxiomSixThroughTen.svelte

--- src/components/axioms/AxiomSixThroughTen.svelte
+++ src/components/axioms/AxiomSixThroughTen.svelte
@@ -6,45 +6,45 @@
   <br />
   <ol>
     <li>
-      <span>6. <span class="text-delta-green">Other than sticking to your cover, never lie to a fellow member of Delta Green.</span></span>
+      <span>6. <span class="text-delta-green">Ngoài việc giữ kín danh tính, không bao giờ được NÓI DỐI bất cứ điều gì với các đặc sứ khác.</span></span>
       <br />
-      <span class="pl-6">  About anything. </span>
+      <span class="pl-6">  Bất cứ điều gì. </span>
       <br />
-      <span class="pl-6">  <span class="text-delta-green">Ever.</span> </span>
+      <span class="pl-6">  <span class="text-delta-green">Không bao giờ.</span> </span>
       <br />
-      <span class="pl-6">  Unless A-cell says otherwise. </span>
+      <span class="pl-6">  Trừ phi Trưởng Lão Hội yêu cầu. </span>
     </li>
     <br />
     <li>
-      <span>7. <span class="text-delta-green">Always lie to anyone who isn't a member of Delta Green.</span></span>
+      <span>7. <span class="text-delta-green">Luôn dối trá đối với ngoại nhân.</span></span>
       <br />
-      <span class="pl-6">  About everything. </span>
+      <span class="pl-6">  Về tất cả mọi thứ. </span>
       <br />
       <span class="pl-6">  <span class="text-delta-green">Always.</span> </span>
       <br />
-      <span class="pl-6">  And make sure your lies are specific and consistent. </span>
+      <span class="pl-6">  Và đảm bảo lời nói dối đó cụ thể và liên tục. </span>
     </li>
     <br />
     <li>
-      <span>8. <span class="text-delta-green">Friendlies can know the truth about an individual operation.</span></span>
+      <span>8. <span class="text-delta-green">Ngoại nhân hỗ trợ được phép biết về nhiệm vụ hiện tại.</span></span>
       <br />
-      <span class="pl-6">  They can never know the <span class="text-delta-green">truth</span> about Delta Green. </span>
+      <span class="pl-6">  Họ không bao giờ được phép biết <span class="text-delta-green">truth</span> về Trấn Vũ Hội. </span>
     </li>
     <br />
     <li>
-      <span>9. <span class="text-delta-green">Always scout your destination, no matter how secure you think it is.</span></span>
+      <span>9. <span class="text-delta-green">Luôn trinh sát trước địa điểm mục tiêu, bất kể ngươi nghĩ nó an toàn như nào.</span></span>
       <br />
-      <span class="pl-6">  Never bring the whole team to do surveillance. </span>
+      <span class="pl-6">  Không bao giờ được mang cả đội đi trinh sát. </span>
       <br />
-      <span class="pl-6">  Be <span class="text-delta-green">inconspicuous.</span> </span>
+      <span class="pl-6">  Hãy luôn <span class="text-delta-green">bí mật.</span> </span>
       <br />
-      <span class="pl-6">  See the enemy first. </span>
+      <span class="pl-6">  Phát hiện ra ai là địch trước. </span>
     </li>
     <br />
     <li>
-      <span>10. <span class="text-delta-green">Always perform a counter-surveillance check before you camp down and before you move.</span></span>
+      <span>10. <span class="text-delta-green">Luôn thiết lập phản trinh sát trước khi dừng chân và di chuyển.</span></span>
       <br />
-      <span class="pl-6">  Perfect <span class="text-delta-green">paranoia</span> is perfect awareness. </span>
+      <span class="pl-6">  Đa nghi <span class="text-delta-green">hoàn toàn</span> là hoàn toàn bình an. </span>
     </li>
   </ol>
 </Typewriter>
